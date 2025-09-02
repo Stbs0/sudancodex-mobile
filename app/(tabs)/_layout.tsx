d6d5bf69f62db420962441cad4f74bafe72a09db
Mohammed Ibrahim Mahmoud <mohammedjrt@gmail.com>
2025-09-02 21:28:39 +0300

    feat: add tabs to tabbar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,19 +1,15 @@
-import FontAwesome from "@expo/vector-icons/FontAwesome";
-import { Link, Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
 import React from "react";
-import { Pressable } from "react-native";
-
-import { useColorScheme } from "@/components/useColorScheme";
-import Colors from "@/constants/Colors";
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>["name"];
+  name: React.ComponentPropsWithoutRef<typeof Ionicons>["name"];
+
   color: string;
 }) {
   return (
-    <FontAwesome
-      size={28}
+    <Ionicons
       style={{ marginBottom: -3 }}
       {...props}
     />
@@ -21,42 +17,51 @@ function TabBarIcon(props: {
 }
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        headerShown: false,
       }}>
       <Tabs.Screen
         name='index'
         options={{
-          title: "Tab One",
-          tabBarIcon: ({ color }) => (
+          title: "Home",
+
+          tabBarIcon: (props) => (
             <TabBarIcon
-              name='code'
-              color={color}
+              name='home'
+              {...props}
             />
-          ),
-          headerRight: () => (
-            <Link
-              href='/modal'
-              asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name='info-circle'
-                    size={25}
-                    color={Colors[colorScheme ?? "light"].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
           ),
         }}
       />
-     
+      <Tabs.Screen
+        name='drugList'
+        options={{
+          title: "Drug List",
+          tabBarLabel: "Drug List",
+
+          tabBarIcon: (props) => (
+            <TabBarIcon
+              name='list'
+              {...props}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name='profile'
+        options={{
+          title: "Profile",
+          tabBarLabel: "Profile",
+          tabBarIcon: (props) => (
+            <TabBarIcon
+              name='person'
+              {...props}
+            />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
